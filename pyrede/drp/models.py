8e7bd5fc3344800c6f230a9a903291b64aeec7fc
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-01-17 15:09:02 +0100

    If we use cache we have to import cache, grrr !!!!

--- pyrede/drp/models.py
+++ pyrede/drp/models.py
@@ -18,6 +18,7 @@
 """
 drp Models for Pyrede
 """
+from django.core.cache import cache
 from django.db import models
 from django.db.models.signals import post_save
 from django.db.models.signals import post_delete
