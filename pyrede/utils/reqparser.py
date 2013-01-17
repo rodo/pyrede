aa8d339637a6f075cf0207e4fa5d41f9a35325d0
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-01-17 10:41:38 +0100

    Increase limit

--- pyrede/utils/reqparser.py
+++ pyrede/utils/reqparser.py
@@ -16,7 +16,7 @@
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-def requ_parser(requirements, limit=20):
+def requ_parser(requirements, limit=30):
     """
     Parse the content of a requirements.txt
 
