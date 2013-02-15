9769baac35bfe052f527882ae42ec764795ea353
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-02-15 11:59:08 +0100

    Remove wrong test

--- pyrede/drp/tests/views.py
+++ pyrede/drp/tests/views.py
@@ -122,34 +122,6 @@ class ViewsTests(TestCase):  # pylint: disable-msg=R0904
         self.assertEqual(result['result'], 1)
         self.assertEqual(result['found'], 2)
 
-    def test_adddispack(self):
-        """
-        Call with all good datas
-        """
-        dist = Distribution.objects.create(name='Foo',
-                                           version_name='Lorem',
-                                           version_number='1.1')
-
-        pack = Package.objects.create(name='aeHohee1',
-                                      latest_version='1.0.0',
-                                      link='http://www.foo.bar',
-                                      description='lorem ipsum')
-        # prepare datas
-        requ = HttpRequest()
-        requ.method = 'POST'
-        requ.META = {'HTTP_REFERER': 'FOO_SERVER_NAME'}
-        requ.POST = {'distribution': str(dist.id),
-                     'name': 'fooname',
-                     'referer': 'fooname',
-                     'version': '1.2.3-a',
-                     'package_version': '1.2.3'}
-
-        #  action
-        result = views.adddispack(requ, pack.name)
-        #  asserts
-        self.assertEqual(result.status_code, 200)  # pylint: disable-msg=E1103
-        # one dispack was created
-        self.assertEqual(DisPack.objects.all().count(), 1)
 
     def test_analyze(self):
         """
