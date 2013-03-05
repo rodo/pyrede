760b6c2c35e5e13221f38275bb2939ae9c6deaf8
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-03-05 14:43:48 +0100

    Add test to increase coverage

--- pyrede/drp/tests/__init__.py
+++ pyrede/drp/tests/__init__.py
@@ -3,6 +3,7 @@ from pyrede.drp.tests.utils import UtilsTests
 from pyrede.drp.tests.package import PackageTests
 from pyrede.drp.tests.packageversion import PackageVersionTests
 from pyrede.drp.tests.dispack import DisPackTests
+from pyrede.drp.tests.debianitp import DebianItpTests
 from pyrede.drp.tests.command import CommandTests
 from pyrede.drp.tests.views import ViewsTests
 from pyrede.drp.tests.templatetags import TemplateTagsTests
